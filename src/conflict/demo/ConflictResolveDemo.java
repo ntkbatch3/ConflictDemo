2ef008fec8a8a5510c212ca0d1911bbb8f98e429
noort <noort@10.1.10.35>
2021-03-25 20:09:30 -0400

    updated script

--- src/conflict/demo/ConflictResolveDemo.java
+++ src/conflict/demo/ConflictResolveDemo.java
@@ -4,10 +4,11 @@ public class ConflictResolveDemo {
 	
 	public static void main(String[] args) {
 
-		int numOneABC = 10;
-		int numTwoXYZ = 20;
+		int nOne = 10;
+		int nTwo = 20;
 
-		System.out.println(numOneABC + numTwoXYZ);
+	
+		System.out.println(nOne + nTwo);
 
 		
 	}

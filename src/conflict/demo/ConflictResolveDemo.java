40a2d3a27b4bc9b7b6782f4df50c91eb8edbd06f
ntkbatch3 <81076004+ntkbatch3@users.noreply.github.com>
2021-03-25 20:11:03 -0400

    Update ConflictResolveDemo.java

--- src/conflict/demo/ConflictResolveDemo.java
+++ src/conflict/demo/ConflictResolveDemo.java
@@ -4,11 +4,11 @@ public class ConflictResolveDemo {
 	
 	public static void main(String[] args) {
 
-		int nOne = 10;
-		int nTwo = 20;
+		int n1 = 10;
+		int n2 = 20;
 
 	
-		System.out.println(nOne + nTwo);
+		System.out.println(n1 + n2);
 
 		
 	}
